cb14676cf35256de96f557e6ee8964dce36e6b0f
Arreolax <ArreolaxStudios@hotmail.com>
2026-02-06 07:54:29 -0600

    fix

--- routes/tickets.routes.js
+++ routes/tickets.routes.js
@@ -2,28 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const tickets = [
-    {
-      fecha: "01/01/2026 10:30:00",
-      nombre_cliente: "Juan Pérez",
-      folio: "432rd3efef",
-      metodo_pago: "Efectivo",
-      productos: [
-        { cantidad: 2, total: 199.98 },
-        { cantidad: 2, total: 99.99 },
-      ],
-    },
-    {
-      fecha: "02/01/2026 18:45:00",
-      nombre_cliente: "María López",
-      folio: "98asd76asd",
-      metodo_pago: "Tarjeta",
-      productos: [
-        { cantidad: 3, total: 300.0 },
-        { cantidad: 1, total: 150.0 },
-      ],
-    },
-  ];
+  const tickets = [ { fecha: "01/01/2026 10:30:00", nombre_cliente: "Juan Pérez", folio: "432rd3efef", metodo_pago: "Efectivo", productos: [ { cantidad: 2, total: 199.98 }, { cantidad: 2, total: 99.99 }, ], }, { fecha: "02/01/2026 18:45:00", nombre_cliente: "María López", folio: "98asd76asd", metodo_pago: "Tarjeta", productos: [ { cantidad: 3, total: 300.0 }, { cantidad: 1, total: 150.0 }, ], }, { fecha: "03/01/2026 12:55:00", nombre_cliente: "Carlitos Lechuga", folio: "4326482322", metodo_pago: "Efectivo", productos: [ { cantidad: 3, total: 300.0 }, { cantidad: 6, total: 500.0 }, ], }, ];
 
   res.render('tickets/tickets', {
     tickets,
